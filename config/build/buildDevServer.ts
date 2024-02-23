6e7fd17bfa7f09cccbb5a402118fd0f9faceb32a
Mattew <shmelev.develop@mail.ru>
2024-02-23 20:19:01 +0300

    scss themes added

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -3,6 +3,7 @@ import type {Configuration as DevServerConfiguration} from 'webpack-dev-server'
 export default function buildDevServer(options:buildOptions) : DevServerConfiguration{
     return {
         port:options.port,
-        open:true
+        open:true,
+        historyApiFallback:true
     }
 }
