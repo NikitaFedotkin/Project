616198b50f53acedc65de5bd54b3aefc06701f60
NikitaFedotkin <149877456+NikitaFedotkin@users.noreply.github.com>
2024-02-24 19:05:17 +0300

    routeConfig edit woth CardPage

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,23 +1,30 @@
-import {RouteProps} from "react-router-dom";
-import {MainPage} from "pages/MainPage";
-import {AboutPage} from "pages/AboutPage";
+import { RouteProps } from "react-router-dom";
+import { MainPage } from "pages/MainPage";
+import { AboutPage } from "pages/AboutPage";
+import { CardPage } from "pages/CardPage";
 
-export enum AppRoutes{
-    MAIN ='main',
-    ABOUT='about'
+export enum AppRoutes {
+  MAIN = "main",
+  ABOUT = "about",
+  CARD = "card",
 }
-export const RoutePath:Record<AppRoutes, string> = {
-    [AppRoutes.MAIN]:'/',
-    [AppRoutes.ABOUT]:'/about'
-}
+export const RoutePath: Record<AppRoutes, string> = {
+  [AppRoutes.MAIN]: "/",
+  [AppRoutes.ABOUT]: "/about",
+  [AppRoutes.CARD]: "/card",
+};
 
-export const RouteConfig:Record<AppRoutes, RouteProps>={
-    [AppRoutes.MAIN]:{
-        path:RoutePath.main,
-        element:<MainPage/>
-    },
-        [AppRoutes.ABOUT]:{
-        path:RoutePath.about,
-        element:<AboutPage/>
-    }
-}
+export const RouteConfig: Record<AppRoutes, RouteProps> = {
+  [AppRoutes.MAIN]: {
+    path: RoutePath.main,
+    element: <MainPage />,
+  },
+  [AppRoutes.ABOUT]: {
+    path: RoutePath.about,
+    element: <AboutPage />,
+  },
+  [AppRoutes.CARD]: {
+    path: RoutePath.card,
+    element: <CardPage />,
+  },
+};
